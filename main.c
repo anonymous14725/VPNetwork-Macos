14c4cca3080a9144d68b20d7eb2455e1b6366fe1
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-08-06 04:05:02 -0700

    main file for VPNetwork

--- main.c
+++ main.c
@@ -36,5 +36,5 @@ int main(){
    	printf(red "This application requires root access" set);
     	printf(set cyan "\nPlease use the sudo -s or su command" set "\n");
 	run();
-	return 0;
+	return 0; 
 }
