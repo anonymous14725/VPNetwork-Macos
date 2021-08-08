14950f982bc5574e3a59a66a9f65c221145a2b9f
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-08-08 04:27:36 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
-#include "tklib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void run(void);
 
 int main(){
-   	printf(red "This application requires root access" set);
-    	printf(set cyan "\nPlease use the sudo -s or su command" set "\n");
+   	printf("\x1B[31m" "This application requires root access" "\x1B[0m");
+    	printf("\x1B[0m" "\x1B[36m" "\nPlease use the sudo -s or su command" "\x1B[0m" "\n");
 	run();
 	return 0; 
+}
+
+
+void run(void){
+    system("wish Tk.tcl");
+    remove("Tk.tcl");
 }
