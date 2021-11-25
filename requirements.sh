6ce94e2512a8033dbe49144b6bf7438981aa4101
cunknown <48595746+anonymous14725@users.noreply.github.com>
2021-11-25 06:59:23 -0800

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Check to see if Homebrew is installed, and install it if it is not
 command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew Now"; \
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; }
 
 if brew ls --versions macosvpn > /dev/null; then
   # The package is installed
